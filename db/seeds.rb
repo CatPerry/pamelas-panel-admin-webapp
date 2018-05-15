6a30eb0aa4c51be857a1eb587b09765ea7992f25
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-05-14 21:38:35 -0400

    randomized Teacher degrees and added image tag to students and teachers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 prng = Random.new
-arr = ["Phd", "Masters", "Bachelors"]
-randDeg = arr.sample
+randDeg = ["Phd", "Masters", "Bachelors"]
 
 1.times do |admin|
   Admin.create!(
@@ -19,11 +18,11 @@ puts "You just created 1 admin"
 
 10.times do |teacher|
   Teacher.create!(
-    first_name: "First Name #{teacher}",
-    last_name: "Last Name #{teacher}",
+    first_name: "#{teacher}",
+    last_name: "#{teacher}",
     age: prng.rand(25..75),
     salary: prng.rand(45000..80000),
-    education: "#{randDeg}",
+    education: "#{randDeg.sample}",
     image: "http://placehold.it/200x200",
     # teacher_id: prng.rand(10000000...19999999)
   )
@@ -33,8 +32,8 @@ puts "You just created 10 teachers"
 
 10.times do |student|
   Student.create!(
-    first_name: "First Name #{student}",
-    last_name: "Last Name #{student}",
+    first_name: "#{student}",
+    last_name: "#{student}",
     age: prng.rand(5..18),
     education: prng.rand(1..12),
     image: "http://placehold.it/200x200",
